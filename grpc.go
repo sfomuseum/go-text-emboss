24a508295c73f3c8ca0db5f772ab4dbd5c7ff8f4
Aaron Straup Cope <thisisaaronland@users.noreply.github.com>
2023-10-23 15:32:12 -0700

    update vendor deps; update to support TLS flags (#3)
    Co-authored-by: sfomuseumbot <sfomuseumbot@localhost>

--- grpc.go
+++ grpc.go
@@ -2,15 +2,20 @@ package emboss
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"io"
+	"io/ioutil"
 	_ "log"
 	"net/url"
 	"os"
 	"path/filepath"
+	"strconv"
 
 	emboss_grpc "github.com/sfomuseum/go-text-emboss/grpc"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 type GrpcEmbosser struct {
@@ -31,10 +36,64 @@ func NewGrpcEmbosser(ctx context.Context, uri string) (Embosser, error) {
 		return nil, fmt.Errorf("Failed to parse URI, %w", err)
 	}
 
+	q := u.Query()
+
+	q_tls_cert := q.Get("tls-certificate")
+	q_tls_key := q.Get("tls-key")
+	q_tls_ca := q.Get("tls-ca-certificate")
+	q_tls_insecure := q.Get("tls-insecure")
+
 	addr := u.Host
 
-	var opts []grpc.DialOption
-	opts = append(opts, grpc.WithInsecure())
+	opts := make([]grpc.DialOption, 0)
+
+	if q_tls_cert != "" && q_tls_key != "" {
+
+		cert, err := tls.LoadX509KeyPair(q_tls_cert, q_tls_key)
+
+		if err != nil {
+			return nil, fmt.Errorf("Failed to load TLS pair, %w", err)
+		}
+
+		tls_config := &tls.Config{
+			Certificates: []tls.Certificate{cert},
+		}
+
+		if q_tls_ca != "" {
+
+			ca_cert, err := ioutil.ReadFile(q_tls_ca)
+
+			if err != nil {
+				return nil, fmt.Errorf("Failed to create CA certificate, %w", err)
+			}
+
+			cert_pool := x509.NewCertPool()
+
+			ok := cert_pool.AppendCertsFromPEM(ca_cert)
+
+			if !ok {
+				return nil, fmt.Errorf("Failed to append CA certificate, %w", err)
+			}
+
+			tls_config.RootCAs = cert_pool
+
+		} else if q_tls_insecure != "" {
+
+			v, err := strconv.ParseBool(q_tls_insecure)
+
+			if err != nil {
+				return nil, fmt.Errorf("Failed to parse ?tls-insecure= parameter, %w", err)
+			}
+
+			tls_config.InsecureSkipVerify = v
+		}
+
+		tls_credentials := credentials.NewTLS(tls_config)
+		opts = append(opts, grpc.WithTransportCredentials(tls_credentials))
+
+	} else {
+		opts = append(opts, grpc.WithInsecure())
+	}
 
 	conn, err := grpc.Dial(addr, opts...)
 
